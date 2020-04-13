dc2ec16878cbe95c1f58f8086ab43b3772b73a18
umssung <em6721@naver.com>
2020-04-14 08:39:57 +0900

    *paging 기능 추가

--- speedrecyclerviewkotlin/app/src/main/java/com/test/speedrecyclerviewkotlin/MainActivity.kt
+++ speedrecyclerviewkotlin/app/src/main/java/com/test/speedrecyclerviewkotlin/MainActivity.kt
@@ -2,7 +2,9 @@ package com.test.speedrecyclerviewkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,22 +14,69 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var page = 1
+    var isLoading = false
+    val limit = 10
+    var people = arrayListOf<Person>()
+
+    lateinit var adapter: PersonAdapter
+    lateinit var layoutManager2: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val people = arrayListOf<Person>()
-        for (i in 0..30) {
-            people.add(Person("홍길동 $i", 10))
-        }
+        layoutManager2 = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager2
+        getPage()
 
 
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity) // == recycler_view.layoutManager
-            adapter = PersonAdapter(people) {person ->                      // == recycle_view.adapter
-                Toast.makeText(this@MainActivity, "$person", Toast.LENGTH_SHORT).show()
+        recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0) {
+                    val visibleItemCount = layoutManager2.childCount
+                    val pastVisibleItem =
+                        layoutManager2.findFirstCompletelyVisibleItemPosition()
+                    val total = adapter.itemCount
+
+                    if (!isLoading) {
+                        if ((visibleItemCount + pastVisibleItem) >= total) {
+                            page++
+                            getPage()
+                        }
+                    }
+                }
+                super.onScrolled(recyclerView, dx, dy)
             }
+        })
+    }
+
+    fun getPage() {
+        isLoading = true
+        progressBar.visibility = View.VISIBLE
+        val start = (page - 1) * limit
+        val end = (page) * limit
+
+        for (i in start..end) {
+            people.add(Person("홍길동 $i", 20))
         }
+
+        Handler().postDelayed({
+            if (::adapter.isInitialized) {
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(this, "생성됨: $start -> $end", Toast.LENGTH_SHORT).show()
+                adapter = PersonAdapter(people) {person ->
+                    // == recycle_view.adapter
+                    Toast.makeText(this, "$person", Toast.LENGTH_SHORT).show()
+                }
+                recycler_view.adapter = adapter
+            }
+            isLoading = false
+            progressBar.visibility = View.GONE
+        }, 5000)
+
+
     }
 }
 
@@ -50,4 +99,5 @@ class PersonAdapter(val items: List<Person>, private val clickListener: (person:
     override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
         holder.binding.person = items[position]
     }
+
 }
