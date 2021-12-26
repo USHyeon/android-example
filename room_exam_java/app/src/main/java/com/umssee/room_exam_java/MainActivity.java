81703e9c930eaf9c7b7f9940bb274fae4ed3fcbe
umssung <em6721@naver.com>
2021-12-26 18:23:13 +0900

    * room - LiveData 사용 - java & kotlin

--- room_exam_java/app/src/main/java/com/umssee/room_exam_java/MainActivity.java
+++ room_exam_java/app/src/main/java/com/umssee/room_exam_java/MainActivity.java
@@ -1,12 +1,15 @@
 package com.umssee.room_exam_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +28,15 @@ public class MainActivity extends AppCompatActivity {
         final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
                 .allowMainThreadQueries()
                 .build();
-        mResultTextView.setText(db.todoDao().getAll().toString());
 
+        // UI 갱신.
+        db.todoDao().getAll().observe(this, todos -> {
+            mResultTextView.setText(todos.toString());
+        });
+
+        // 버튼 클릭시 DB에 insert
         findViewById(R.id.add_button).setOnClickListener(view -> {
             db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-            mResultTextView.setText(db.todoDao().getAll().toString());
         });
     }
 }
--- room_exam_kotlin/app/src/main/java/com/umssee/room_exam_kotlin/MainActivity.kt
+++ room_exam_kotlin/app/src/main/java/com/umssee/room_exam_kotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +21,13 @@ class MainActivity : AppCompatActivity() {
         val todoEdit = findViewById<EditText>(R.id.todo_edit)
         val resultText = findViewById<TextView>(R.id.result_text)
         val addButton = findViewById<Button>(R.id.add_button)
-        resultText.text = db.todoDao().getAll().toString()
+
+        db.todoDao().getAll().observe(this, Observer {
+            resultText.text = it.toString()
+        })
 
         addButton.setOnClickListener {
             db.todoDao().insert(Todo(todoEdit.text.toString()))
-            resultText.text = db.todoDao().getAll().toString()
          }
 
     }
