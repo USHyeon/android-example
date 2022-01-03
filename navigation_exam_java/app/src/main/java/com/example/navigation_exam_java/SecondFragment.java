be47e67eec5abfe8e9a1f6a7574d63611320d214
Eom Book <umsp27@spacetalk.co.kr>
2022-01-03 18:15:16 +0900

    * Navigation Example - java

---
+++ navigation_exam_java/app/src/main/java/com/example/navigation_exam_java/SecondFragment.java
@@ -0,0 +1,35 @@
+package com.example.navigation_exam_java;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // get argument
+        String text = SecondFragmentArgs.fromBundle(getArguments()).getVar();
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setText(text);
+    }
+}
