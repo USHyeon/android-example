0f9a8de5bb19ad0261e54359ab04cd4704dbb875
Eom Book <umsp27@spacetalk.co.kr>
2022-01-03 18:54:46 +0900

    * Navigation Example - kotlin

---
+++ navigation_exam_kotlin/app/src/main/java/com/example/navigation_exam_kotlin/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigation_exam_kotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToSecondFragment("Hello!!Kotlin!!")
+            findNavController().navigate(action)
+        }
+
+
+    }
+}
