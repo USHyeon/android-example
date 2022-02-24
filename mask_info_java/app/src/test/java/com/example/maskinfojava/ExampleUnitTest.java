f71026f1cfa42359107b9812a7b5eff3f84e3911
Eom Book <umsp27@spacetalk.co.kr>
2022-02-24 18:43:55 +0900

    ⚗️ Retrofit 동작 테스트

--- mask_info_java/app/src/test/java/com/example/maskinfojava/ExampleUnitTest.java
+++ mask_info_java/app/src/test/java/com/example/maskinfojava/ExampleUnitTest.java
@@ -4,6 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.maskinfojava.model.StoreInfo;
+import com.example.maskinfojava.repository.MaskService;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +22,22 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void retrofitTest() throws IOException {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MaskService.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
+
+        MaskService service = retrofit.create(MaskService.class);
+
+        Call<StoreInfo> storeInfoCall = service.fetchStoreInfo();
+
+        StoreInfo storeInfo = storeInfoCall.execute().body();
+
+        assertEquals(222, storeInfo.getCount());
+        assertEquals(222, storeInfo.getStores().size());
     }
 }
