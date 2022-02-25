b9a6f4d9e9c3d073af48a96aefed11abeed08fc6
Eom Book <umsp27@spacetalk.co.kr>
2022-02-25 16:11:56 +0900

    🎨 API 연동(실제 데이터)

--- mask_info_java/app/src/main/java/com/example/maskinfojava/MainActivity.java
+++ mask_info_java/app/src/main/java/com/example/maskinfojava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.maskinfojava;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,11 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.maskinfojava.adapter.StoreAdapter;
 import com.example.maskinfojava.model.Store;
+import com.example.maskinfojava.model.StoreInfo;
+import com.example.maskinfojava.repository.MaskService;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +36,24 @@ public class MainActivity extends AppCompatActivity {
         StoreAdapter storeAdapter = new StoreAdapter();
         mainRecyclerView.setAdapter(storeAdapter);
 
-        List<Store> items = new ArrayList<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MaskService.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
 
-        Store store01 = new Store();
-        store01.setName("우리 약국");
-        store01.setAddr("서울시 용산구 한남동");
+        MaskService service = retrofit.create(MaskService.class);
+        Call<StoreInfo> storeInfoCall = service.fetchStoreInfo();
+        storeInfoCall.enqueue(new Callback<StoreInfo>() {
+            @Override
+            public void onResponse(Call<StoreInfo> call, Response<StoreInfo> response) {
+                List<Store> items = response.body().getStores();
+                storeAdapter.updateItems(items);
+            }
 
-        items.add(store01);
-        items.add(store01);
-        items.add(store01);
-        items.add(store01);
-        items.add(store01);
-        items.add(store01);
-
-        storeAdapter.updateItems(items);
-
+            @Override
+            public void onFailure(Call<StoreInfo> call, Throwable t) {
+                Log.e(TAG, "onFailure: ", t);
+            }
+        });
     }
 }
