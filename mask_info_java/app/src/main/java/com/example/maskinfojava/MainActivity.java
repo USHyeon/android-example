884c1ec5c1ff2ca8f0941083cfc7bff067e62f24
Eom Book <umsp27@spacetalk.co.kr>
2022-02-25 19:02:54 +0900

    🎨 ViewModel을 적용하여 UI와 데이터 분리

--- mask_info_java/app/src/main/java/com/example/maskinfojava/MainActivity.java
+++ mask_info_java/app/src/main/java/com/example/maskinfojava/MainActivity.java
@@ -1,32 +1,22 @@
 package com.example.maskinfojava;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.maskinfojava.adapter.StoreAdapter;
-import com.example.maskinfojava.model.Store;
-import com.example.maskinfojava.model.StoreInfo;
-import com.example.maskinfojava.repository.MaskService;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.moshi.MoshiConverterFactory;
+import com.example.maskinfojava.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private MainViewModel mainViewModel;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,13 +30,12 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.actionRefresh:
-
+                mainViewModel.fetchStoreInfo();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,27 +49,10 @@ public class MainActivity extends AppCompatActivity {
         StoreAdapter storeAdapter = new StoreAdapter();
         mainRecyclerView.setAdapter(storeAdapter);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(MaskService.BASE_URL)
-                .addConverterFactory(MoshiConverterFactory.create())
-                .build();
-
-        MaskService service = retrofit.create(MaskService.class);
-        Call<StoreInfo> storeInfoCall = service.fetchStoreInfo();
-        storeInfoCall.enqueue(new Callback<StoreInfo>() {
-            @Override
-            public void onResponse(Call<StoreInfo> call, Response<StoreInfo> response) {
-                List<Store> items = response.body().getStores();
-                storeAdapter.updateItems(items.stream()
-                        .filter(item -> item.getRemainStat() != null)
-                        .collect(Collectors.toList()));
-                getSupportActionBar().setTitle("마스크 재고 있는 곳: " + items.size());
-            }
-
-            @Override
-            public void onFailure(Call<StoreInfo> call, Throwable t) {
-                Log.e(TAG, "onFailure: ", t);
-            }
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mainViewModel.itemLiveData.observe(this, stores -> {
+            storeAdapter.updateItems(stores);
+            getSupportActionBar().setTitle("마스크 재고 있는 곳: " + stores.size());
         });
     }
 }
---
+++ mask_info_java/app/src/main/java/com/example/maskinfojava/viewmodel/MainViewModel.java
@@ -0,0 +1,57 @@
+package com.example.maskinfojava.viewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.maskinfojava.model.Store;
+import com.example.maskinfojava.model.StoreInfo;
+import com.example.maskinfojava.repository.MaskService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class MainViewModel extends ViewModel {
+
+    private static final String TAG = MainViewModel.class.getSimpleName();
+    public MutableLiveData<List<Store>> itemLiveData = new MutableLiveData<>();
+
+    private Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(MaskService.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build();
+
+    private MaskService service = retrofit.create(MaskService.class);
+    private Call<StoreInfo> storeInfoCall = service.fetchStoreInfo();
+
+    public MainViewModel() {
+        fetchStoreInfo();
+    }
+
+    public void fetchStoreInfo() {
+        storeInfoCall.clone().enqueue(new Callback<StoreInfo>() {
+            @Override
+            public void onResponse(Call<StoreInfo> call, Response<StoreInfo> response) {
+                Log.d(TAG, "++ API onResponse");
+                List<Store> items = response.body().getStores().stream()
+                        .filter(item -> item.getRemainStat() != null)
+                        .collect(Collectors.toList());
+                itemLiveData.postValue(items);
+            }
+
+            @Override
+            public void onFailure(Call<StoreInfo> call, Throwable t) {
+                itemLiveData.postValue(Collections.emptyList());
+            }
+        });
+    }
+
+}
