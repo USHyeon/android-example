39a837dda9f5ae7633a00bbcf78951fea9b3d015
umssung <em6721@naver.com>
2022-03-01 20:48:51 +0900

    🔒️ 위치정보 권한 체크

--- mask_info_java/app/src/main/java/com/example/maskinfojava/MainActivity.java
+++ mask_info_java/app/src/main/java/com/example/maskinfojava/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.maskinfojava;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -12,11 +16,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.maskinfojava.adapter.StoreAdapter;
 import com.example.maskinfojava.viewmodel.MainViewModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private MainViewModel mainViewModel;
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,10 +55,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                performAction();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]") // 사용자가 거부했을 때의 메시지
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void performAction() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
+
         RecyclerView mainRecyclerView = findViewById(R.id.mainRecyclerView);
-
         mainRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-
         StoreAdapter storeAdapter = new StoreAdapter();
         mainRecyclerView.setAdapter(storeAdapter);
 
